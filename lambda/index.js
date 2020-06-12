6a9f9d1f705eb98b7bd64235b4091459b7f0d471
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-06-12 19:44:12 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -21,7 +21,7 @@ const CheckScheduleIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'CheckScheduleIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'Hello World!';
+        const speakOutput = 'Hello Andrew!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
