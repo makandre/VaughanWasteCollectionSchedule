c8a9147d28a584f3d2aaa66b46e24036535d1b52
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-07-11 00:32:42 +0000

    updated lambda/impl.js.

---
+++ lambda/impl.js
@@ -0,0 +1,30 @@
+const currentWeekNumber = require('current-week-number');
+
+const getWeek = () => {
+    
+    const date = new Date();
+    
+    // if Sunday, advance by 1 day to account for the week starting on Monday
+    if (date.getDay() === 0)
+        date.setDate(date.getDate() + 1);
+        
+    const week = currentWeekNumber(date);
+    const colour = week % 2 === 0 ? 'blue' : 'yellow';
+    
+    return { week, colour };
+};
+
+const checkSchedule = () => {
+    
+    const week = getWeek();
+        
+    return `Week ${week.week} is a ${week.colour} week.`;
+};
+
+const checkPickup = () => {
+
+    const week = getWeek();
+        
+    return `It is garbage pickup week if you live in a ${week.colour} zone.`;
+};
+
