212d12404a92f1f525b7fa80bf2305d8fe778700
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-07-11 01:07:36 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -3,7 +3,6 @@
 // session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
 const impl = require('./impl')
-//const currentWeekNumber = require('current-week-number');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
@@ -31,20 +30,6 @@ const ZoneIntentHandler = {
             .getResponse();
     }
 };
-
-/*const getWeek = () => {
-    
-    const date = new Date();
-    
-    // if Sunday, advance by 1 day to account for the week starting on Monday
-    if (date.getDay() === 0)
-        date.setDate(date.getDate() + 1);
-        
-    const week = currentWeekNumber(date);
-    const colour = week % 2 === 0 ? 'blue' : 'yellow';
-    
-    return { week, colour };
-};*/
 
 const CheckScheduleIntentHandler = {
     canHandle(handlerInput) {
